aa7d4770d07c9416a651bbebbbd15aeeb5e7ee21
Priya Mishra <connect.priyamishra@gmail.com>
2019-11-13 01:48:00 +0530

    Initial commit

---
+++ aws-exports.js
@@ -0,0 +1,8 @@
+const awsConfig = {
+  identityPoolId: 'us-west-2:b44d30a4-4d1c-4612-8594-0abdf72191b1',
+  region: 'us-west-2',
+  userPoolId: 'us-west-2_l9F5aE2lI',
+  userPoolWebClientId: '5p22s7p2l7052ioiiffaommjgr'
+}
+
+export default awsConfig;
